c964b3e5cdc82960f98e661526a0b7a5e0a804a9
easytseng <easytseng@kpmg.com.tw>
2022-01-25 11:46:19 +0800

    參數抽出來放到txt

--- HealthCheck/Program.cs
+++ HealthCheck/Program.cs
@@ -19,16 +19,23 @@ var holidayList = list.ToArray();
 
 var today = DateTime.Now.ToString("yyyy/MM/dd");
 
-int dayOfWeek = (int)DateTime.Now.DayOfWeek;
-String jsonStr;
-if (holidayList.Contains(today))
-{
-    jsonStr = "{    \"EmpID\": \"16210\",\"RecordDate\": \"" + DateTime.Now.ToString("MM/dd yyyy") + " 00:00:00\",\"WorkPlaceAM\": \"住家\",\"WorkPlacePM\": \"住家\",\"WorkProject\": \"休/例假\",\"Symptom\": \"false\"}"; 
+
+
+
+string jsonString = File.ReadAllText("config.txt");
+JObject json = JObject.Parse(jsonString);
+JObject requestJson;
+if(holidayList.Contains(today)){
+    requestJson = json.GetValue("HolidaySetting").ToObject<JObject>();
 }
 else
 {
-    jsonStr = "{    \"EmpID\": \"16210\",\"RecordDate\": \"" + DateTime.Now.ToString("MM/dd yyyy") + " 00:00:00\",\"WorkPlaceAM\": \"辦公室\",\"WorkPlacePM\": \"辦公室\",\"WorkProject\": \"顧問專案\",\"Symptom\": \"false\",\"Seat\": \"固定座位\",\"SeatNo\": \"\"}";
+    requestJson = json.GetValue("WorkingdaySetting").ToObject<JObject>();
 }
+requestJson.Add("RecordDate", DateTime.Now.ToString("MM/dd yyyy") + " 00:00:00");
+
+String jsonStr =  requestJson.ToString();
+
 var content = new StringContent(jsonStr, Encoding.UTF8, "application/json");
 var result = client.PostAsync("https://smartim.kpmg.com.tw/HealthCheck/Web/SelfCheck/Create", content).Result;
 Console.WriteLine(result.StatusCode);
